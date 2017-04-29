888f9c7249a1decb52ee59afdc3bbb76f825a10e
coolklokan <coolklokan@gmail.com>
2017-04-29 14:37:12 +0200

    file-security

--- index.php
+++ index.php
@@ -5,10 +5,14 @@ use presenter\Presenter;
 
 require "startup.php";
 
-try {
-    $file = new File($_SERVER["REQUEST_URI"]);
-    if ($file->read()) exit();
+$requesturi = $_SERVER["REQUEST_URI"];
 
+try {
+    if (strpos($requesturi, ".")) {
+        $file = new File($requesturi);
+        $file->read();
+        exit();
+    }
     echo Presenter::route($_POST, $_GET, $_FILES, $_SERVER, $_SESSION)->output();
 } catch (Exception $ex) {
     throw $ex;
--- model/File.php
+++ model/File.php
@@ -5,6 +5,7 @@ namespace model;
 
 
 use model\settings\AppSettings;
+use model\settings\HeaderTypes;
 
 final class File {
     private $filename;
@@ -17,9 +18,14 @@ final class File {
     }
 
     public function read(): bool {
-        if (!$this->isValidExtension()) throw new \Exception("Invalid file extension");
-        if (!is_file($this->filename)) return false;
-        header(HTTPHeaderTypes::OK);
+        if (!$this->isValidExtension()) {
+            header(HeaderTypes::FORBIDDEN);
+            return false;
+        }else if (!is_file($this->filename)) {
+            header(HeaderTypes::NOT_FOUND);
+            return false;
+        }
+        header(HeaderTypes::OK);
         header("Cache-Control: public"); // needed for internet explorer
         header("Content-Transfer-Encoding: Binary");
         header("Content-Length:" . filesize($this->filename));
