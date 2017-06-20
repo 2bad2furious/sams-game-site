dc07904db502dddbf8efcc1029e79aee6f07c5ed
coolklokan <coolklokan@gmail.com>
2017-06-20 09:23:31 +0200

    added User::get

--- model/user/User.php
+++ model/user/User.php
@@ -7,9 +7,9 @@ namespace model\user;
 use model\settings\AppSettings;
 use model\utility\Db;
 
-final class User {
+final class User implements \Serializable, \JsonSerializable {
     private static $extractedUser;
-
+    private $id;
     private $username;
     private $email;
     private $password;
@@ -20,7 +20,7 @@ final class User {
         $data = Db::instance()->single("SELECT * FROM user WHERE username LIKE ?", array($username));
         if ($data)
             if (password_verify($password, $data["password"])) {
-                $user = new User($data["username"], $data["password"], $data["email"],$_SERVER["REMOTE_ADDR"]);
+                $user = new User($data["user_id"], $data["username"], $data["password"], $data["email"], $_SERVER["REMOTE_ADDR"]);
                 $_SESSION["user"] = $user;
                 return $user;
             }
@@ -40,7 +40,7 @@ final class User {
 
         $hashedPassword = self::hash($password);
         $creation = Db::instance()->add("user", array("username" => $username, "password" => $hashedPassword, "email" => $email));
-        if ($creation) return new User($username, $hashedPassword, $email, $ip);
+        if ($creation) return new User($id = Db::instance()->lastInsertId(), $username, $hashedPassword, $email, $ip);
 
         return null;
     }
@@ -52,17 +52,19 @@ final class User {
 
     /**
      * User constructor.
+     * @param int $id
      * @param string $username
      * @param string $password
      * @param string $email
      * @param string $ip
      */
-    private function __construct(string $username, string $password, string $email, string $ip) {
+    private function __construct(int $id, string $username, string $password, string $email, string $ip) {
         $this->username = $username;
         $this->password = $password;
         $this->email = $email;
         $this->ip = $ip;
         $this->time = time();
+        $this->id = $id;
     }
 
     private function exists(): bool {
@@ -123,12 +125,53 @@ final class User {
         unset($_SESSION["user"]);
     }
 
+    public static function getById(int $id): ?User {
+        $data = Db::instance()->single("SELECT * FROM user WHERE user_id = ?", array($id));
+        if ($data) {
+            return new User($data["user_id"], $data["username"], "", $data["email"], "");
+        }
+        return null;
+    }
+
     public function getUsername(): string {
         return $this->username;
     }
 
-    private function setInfo(): void {
-        $data = $this->getInfo();
+    public function getId(): int {
+        return $this->id;
     }
 
+    /**
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize() {
+        // TODO: Implement serialize() method.
+    }
+
+    /**
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized) {
+        // TODO: Implement unserialize() method.
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    function jsonSerialize() {
+        // TODO: Implement jsonSerialize() method.
+    }
 }
