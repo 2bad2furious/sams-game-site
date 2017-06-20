e0d8bc0535c24317ccc65b075c0fd2530e4ac9bd
coolklokan <coolklokan@gmail.com>
2017-06-20 09:24:05 +0200

    Map managing

--- presenter/CreateMapPresenter.php
+++ presenter/CreateMapPresenter.php
@@ -4,9 +4,24 @@
 namespace presenter;
 
 
+use model\form\customforms\MapEditForm;
+use model\Map;
+use model\user\User;
+
 class CreateMapPresenter extends Presenter {
 
     protected function main(): void {
-        
+        $this->getView()->setIsLogged($isLogged = User::isLoggedIn());
+        if (!$isLogged) return;
+
+        $form = new MapEditForm($this->getLang(), $this->getLang()->getCreateMap(), $this->getLang()->getSave(), $_POST, null, null, null, "mapForm");
+
+        $this->getView()->setForm($form);
+
+        $this->getView()->setHasData($hasData = $form->hasData());
+
+        if (!$hasData) return;
+
+        $this->getView()->setMapCreated($mapCreated = Map::createNewMap($form->getJson()));
     }
 }
--- presenter/EditMapPresenter.php
+++ presenter/EditMapPresenter.php
@@ -4,9 +4,42 @@
 namespace presenter;
 
 
+use model\form\customforms\MapEditForm;
+use model\Map;
+use model\user\User;
+
 class EditMapPresenter extends Presenter {
 
     protected function main(): void {
-        // TODO: Implement main() method.
+        $this->getView()->setIsLogged($isLogged = User::isLoggedIn());
+        if (!$isLogged) return;
+
+        $parameters = $this->getView()->getParameters();
+
+        $parameter = "";
+
+        $this->getView()->setParameterExists($parameterExists = (isset($parameters[0]) && $parameter = intval($parameters[0]) && $parameter > 0));
+
+        if (!$parameterExists) return;
+
+        $this->getView()->setExists($exists = Map::exists($parameter));
+
+        if(!$exists) return;
+
+        $this->getView()->setHasRights($hasRights = Map::hasRights($parameter));
+
+        if(!$hasRights) return;
+
+        $mapArray = Map::get($parameter)->serialize();
+
+        $form = new MapEditForm($this->getLang(), $this->getLang()->getCreateMap(), $this->getLang()->getSave(), ($_POST) ? $_POST : $mapArray, null, null, null, "mapForm");
+
+        $this->getView()->setForm($form);
+
+        $this->getView()->setHasData($hasData = $form->hasData());
+
+        if (!$hasData) return;
+
+        $this->getView()->setMapCreated($mapCreated = Map::editMap($parameter, $form->getJson()));
     }
 }
